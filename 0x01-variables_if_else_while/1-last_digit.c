cb2bd15dbaf128b3e9338d7c63f8d3c8903f57f6
meraf00 <meraf000@gmail.com>
2022-04-08 16:56:39 +0300

    Solved

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
     else if (n % 10 == 0)
         printf("and is 0");
     else
-        printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0");
     
     return (0);
 }
