d5f85e872865505e69a67f248019af21f12a2862
meraf00 <meraf000@gmail.com>
2022-04-08 17:24:30 +0300

    Solved

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i != 4 && i != 16)
+			putchar(97 + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
