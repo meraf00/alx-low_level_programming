d221fa1442705e98c39d809b250928453b97b13c
meraf00 <meraf000@gmail.com>
2022-04-08 17:09:42 +0300

    Solved

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	for (int i = 0; i < 26; i++)
+		putchar(97 + i);
+
+	return (0);
+}
