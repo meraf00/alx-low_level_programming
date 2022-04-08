7f30d502c65c81e1955d119805cdcab6cf41c9c4
meraf00 <meraf000@gmail.com>
2022-04-08 18:06:21 +0300

    Solved

---
+++ 0x01-variables_if_else_while/100-print_comb3 copy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{	
+			for (k = j + 1; k < 10; k++)
+			{	
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(48 + k);
+
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+					putchar('\n');
+			}
+		}
+	}
+
+	return (0);
+}
